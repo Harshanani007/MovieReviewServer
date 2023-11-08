710da2df5b0b3f71fc922d3198810d65bd5c8bb3
Harshavardhan Reddy <harshavardhan.vadde@gmail.com>
2023-11-08 11:58:35 +0530

    created a healthcheck controller

---
+++ src/main/java/com/example/server/controllers/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.example.server.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthz")
+    public ResponseEntity<String> healthCheck() {
+        // You can add your custom checks here (e.g., check the database)
+        // If everything is healthy, return a 200 OK response
+        return ResponseEntity.ok("OK");
+    }
+}
